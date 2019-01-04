a428bd308332d6e677eedf802a9663f7670178eb
Sudi_Dav <sudisimbadav@gmail.com>
2019-01-04 13:28:48 +0300

    End of Section 3

--- SharingApp.API/Data/DataContext.cs
+++ SharingApp.API/Data/DataContext.cs
@@ -8,5 +8,6 @@ namespace SharingApp.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
